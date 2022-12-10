a797050b86b7d1e3b54dc2d6a8393a7ae10050ea
aaronsleepy <aaron@kmong.com>
2022-12-10 21:17:42 +0900

    init: first commit

---
+++ SymbolRollerSwiftUI/SymbolRollerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolRollerSwiftUIApp.swift
+//  SymbolRollerSwiftUI
+//
+//  Created by Aaron on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct SymbolRollerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
